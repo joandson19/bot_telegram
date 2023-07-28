1986b4daf4db880c4e5868d643bf22128a4cbaf8
Joandson Bezerra <36518985+joandson19@users.noreply.github.com>
2023-07-28 13:24:27 -0300

    Update bot.py
    * Atualização 28/07/2023 
    -> Adicionado suporte a subpastas
    -> Adicionado botão de voltar (facilita navegação entre pastas)
    -> Pode salva o arquivo diretamente na pasta que está navegando

--- bot_telegram_arquivos/bot.py
+++ bot_telegram_arquivos/bot.py
@@ -22,35 +22,80 @@ authorized_users = set()
 def start(message):
     bot.send_message(chat_id=message.chat.id, text="Olá! Bem-vindo ao bot de acesso a arquivos. Use o comando /listar para ver a lista de arquivos.")
 
-# Handler para o comando /listar
+
+# New global variable to keep track of user's current path
+current_path = {}
+
+# Handler for the command /listar
 @bot.message_handler(commands=['listar'])
 def list_files(message):
     if is_user_authorized(message.chat.id):
-        files = os.listdir(FOLDER_PATH)
+        # Get the current path for the user
+        path = current_path.get(message.chat.id, FOLDER_PATH)
+        files = os.listdir(path)
+
         if files:
             keyboard = InlineKeyboardMarkup(row_width=1)
             for file_name in files:
                 button = InlineKeyboardButton(text=file_name, callback_data=f"baixar:{file_name}")
                 keyboard.add(button)
+            
+            # Add a button for navigating back if the current path is not the root folder
+            if path != FOLDER_PATH:
+                back_button = InlineKeyboardButton(text="↩️ Voltar", callback_data="voltar")
+                keyboard.add(back_button)
+
             bot.send_message(chat_id=message.chat.id, text="Lista de arquivos:", reply_markup=keyboard)
         else:
-            bot.send_message(chat_id=message.chat.id, text="A pasta está vazia.")
+            # Folder is empty, show "↩️ Voltar" button to navigate back
+            keyboard = InlineKeyboardMarkup(row_width=1)
+            back_button = InlineKeyboardButton(text="↩️ Voltar", callback_data="voltar")
+            keyboard.add(back_button)
+            bot.send_message(chat_id=message.chat.id, text="A pasta está vazia.", reply_markup=keyboard)
     else:
         bot.send_message(chat_id=message.chat.id, text="Você não está autorizado a realizar esta ação.")
 
-# Handler para o comando /baixar
-@bot.callback_query_handler(func=lambda call: call.data.startswith('baixar:'))
-def download_file(call):
+
+# Handler for the command /voltar (to navigate back to the parent folder)
+@bot.callback_query_handler(func=lambda call: call.data == "voltar")
+def navigate_back_button(call):
     if is_user_authorized(call.message.chat.id):
-        file_name = call.data.split(':')[1]
-        file_path = os.path.join(FOLDER_PATH, file_name)
-        if os.path.isfile(file_path):
-            with open(file_path, 'rb') as file:
-                bot.send_document(call.message.chat.id, file)
-        else:
-            bot.send_message(chat_id=call.message.chat.id, text="Arquivo não encontrado.")
+        user_id = call.message.chat.id
+        if user_id in current_path and current_path[user_id] != FOLDER_PATH:
+            current_path[user_id] = os.path.dirname(current_path[user_id])  # Go up one level
+        list_files(call.message)  # Show the contents of the parent directory
     else:
-        bot.send_message(chat_id=call.message.chat.id, text="Você não está autorizado a realizar esta ação.")        
+        bot.send_message(chat_id=call.message.chat.id, text="Você não está autorizado a realizar esta ação.")
+
+
+# Handler for the command /baixar and navigating into folders
+@bot.callback_query_handler(func=lambda call: call.data.startswith('baixar:'))
+def download_file_or_navigate(call):
+    if is_user_authorized(call.message.chat.id):
+        user_id = call.message.chat.id
+        data = call.data.split(':')
+        action = data[0]
+        target = data[1]
+
+        # Handle navigation into folders
+        if os.path.isdir(os.path.join(current_path.get(user_id, FOLDER_PATH), target)):
+            # Update the current path for the user
+            if user_id not in current_path:
+                current_path[user_id] = FOLDER_PATH
+            current_path[user_id] = os.path.join(current_path[user_id], target)
+            list_files(call.message)  # Show the contents of the new directory
+            return
+
+        # Handle file download
+        if action == 'baixar':
+            file_path = os.path.join(current_path.get(user_id, FOLDER_PATH), target)
+            if os.path.isfile(file_path):
+                with open(file_path, 'rb') as file:
+                    bot.send_document(call.message.chat.id, file)
+            else:
+                bot.send_message(chat_id=call.message.chat.id, text="Arquivo não encontrado.")
+    else:
+        bot.send_message(chat_id=call.message.chat.id, text="Você não está autorizado a realizar esta ação.")
 
 # Handler para receber um arquivo
 @bot.message_handler(content_types=['document'])
@@ -88,8 +133,11 @@ def process_confirmation(call):
                 # Recupera o nome do arquivo
                 file_name = file_info.file_path.split('/')[-1]
 
-                # Salva o arquivo na pasta
-                file_path = os.path.join(FOLDER_PATH, file_name)
+                # Obtém o caminho da pasta onde o arquivo deve ser salvo (usando o current_path)
+                current_folder_path = current_path.get(chat_id, FOLDER_PATH)
+
+                # Salva o arquivo na pasta correta
+                file_path = os.path.join(current_folder_path, file_name)
                 with open(file_path, 'wb') as file:
                     file.write(downloaded_file)
 
@@ -101,7 +149,7 @@ def process_confirmation(call):
     else:
         bot.send_message(chat_id=chat_id, text="Você não está autorizado a realizar esta ação.")
 
-# Handler para o comando /autenticar
+# Handler for the command /autenticar
 @bot.message_handler(commands=['autenticar'])
 def authenticate(message):
     try:
